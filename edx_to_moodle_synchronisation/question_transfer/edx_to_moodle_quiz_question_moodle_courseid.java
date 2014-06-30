83c717b775c3ef58a6956b6294b55c5dfebe6173
Rajarshi Sarkar <rajarshi@rajarshi-HP-Pavilion-dv6-Notebook-PC>
2014-06-30 18:41:06 +0530

    Question Transfer Bug Removed

--- edx_to_moodle_synchronisation/question_transfer/edx_to_moodle_quiz_question_moodle_courseid.java
+++ edx_to_moodle_synchronisation/question_transfer/edx_to_moodle_quiz_question_moodle_courseid.java
@@ -12,7 +12,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
-public class edx_to_moodle_send_pdf_get_courseid {
+public class edx_to_moodle_quiz_question_moodle_courseid {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost/moodle";
     static final String USER = "root";
@@ -20,33 +20,43 @@ public class edx_to_moodle_send_pdf_get_courseid {
     public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException, NoSuchAlgorithmException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = null;
+        Connection conn1 = null;
+        Connection conn2 = null;
         conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        conn1 = DriverManager.getConnection(DB_URL, USER, PASS);
+        conn2 = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement(); //mysql statement
+        Statement stmt1 = conn1.createStatement(); //mysql statement
+        Statement stmt2 = conn2.createStatement(); //mysql statement
         //To connect to mongodb server
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         //Now connect to your databases
-        DB db = mongoClient.getDB("xcontent");
-        DBCollection coll = db.getCollection("fs.files");
-        BasicDBObject query = new BasicDBObject("_id.category", "asset").append("_id.tag", "c4x");
+        DB db = mongoClient.getDB("xmodule");
+        DBCollection coll = db.getCollection("modulestore");
+        String question_to_be_sent = "";
+        BasicDBObject query = new BasicDBObject("_id.category", "problem");
         DBCursor cursor = coll.find(query);
         String result = "", resultcopy = "";
+        int subsection_it_was_added = 0;
+        int section_it_was_added = 0;
         while (cursor.hasNext()) {
             DBObject tobj = cursor.next();
             result = tobj.get("_id").toString();
             resultcopy = result;
+            question_to_be_sent = tobj.get("metadata").toString();
         }
         String[] temp1 = result.split(":");
         String resa = temp1[3];
         String[] resb = resa.split(",");
         String resc = resb[0];
         String resd = resc.substring(2, resc.length() - 2);
-        System.out.println(resd);
-        String[] temp2 = resultcopy.split(":");
-        String rese = temp2[5];
-        String[] resf = rese.split(",");
-        String resg = resf[0];
-        String resh = resg.substring(2, resg.length() - 2);
-        //System.out.println(resh);
+        String sql = "select * from mdl_course where idnumber='" + resd + "'";
+        ResultSet rs = stmt.executeQuery(sql);
+        int courseid = 0;
+        while (rs.next()) {
+            courseid = rs.getInt("id");
+        }
+        System.out.print(courseid);
         cursor.close();
     }
 }
